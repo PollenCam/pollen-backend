48bf2efd5fc94cca2fc9fa60de2bb6a314beb0be
Brian Kung <brian@callmekung.com>
2018-06-20 14:54:11 -0500

    Adds minimal enum specs

--- spec/models/membership_spec.rb
+++ spec/models/membership_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Membership, type: :model do
   before(:all) { @membership = Membership.new }
 
-  %w{ user event role }.each do |attribute|
+  %w{ user event role attendee? owner? }.each do |attribute|
     it("Responds to ##{attribute}") { @membership.send(attribute) }
   end
 end
