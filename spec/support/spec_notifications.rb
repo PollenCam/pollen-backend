43d8b21fb2c81c2827ed41ca8fdaaa60a72376b4
Brian Kung <brian@callmekung.com>
2018-07-14 18:28:31 -0500

    Flips spec notifier condition to catch errors outside of specs

--- spec/support/spec_notifications.rb
+++ spec/support/spec_notifications.rb
@@ -2,14 +2,14 @@ RSpec.configure do |config|
   next unless (RUBY_PLATFORM =~ /darwin/ && ENV['SPEC_NOTIFICATIONS'] =~ /\Atrue\Z/i)
 
   config.after(:suite) do
-    any_failures = RSpec.world.filtered_examples.values.flatten.any?(&:exception)
+    all_passing = RSpec.world.filtered_examples.values.flatten.map(&:execution_result).all? { |result| result.status == :passed }
 
-    if any_failures
-      TerminalNotifier.notify('❗️')
-      system("say 'tests failing'")
-    else
+    if all_passing
       TerminalNotifier.notify('✅')
       system("say 'tests passing'")
+    else
+      TerminalNotifier.notify('❗️')
+      system("say 'tests failing'")
     end
   end
 end
