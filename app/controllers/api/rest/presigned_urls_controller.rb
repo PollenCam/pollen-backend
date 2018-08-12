37fa4c0a0e25f8225cfd82c1b151fef4c02f085c
Brian Kung <brian@callmekung.com>
2018-08-12 16:52:59 -0500

    Puts dependencies where they belong

--- app/controllers/api/rest/presigned_urls_controller.rb
+++ app/controllers/api/rest/presigned_urls_controller.rb
@@ -1,6 +1,3 @@
-require 'aws-sdk-s3'
-require 'securerandom'
-
 class Api::Rest::PresignedUrlsController < ApplicationController
   def create
     render json: { presigned_url: PresignedUrl.generate }, status: :ok
--- lib/presigned_url.rb
+++ lib/presigned_url.rb
@@ -1,5 +1,9 @@
+require 'aws-sdk-s3'
+require 'securerandom'
+
 class PresignedUrl
   def self.generate
+    # TODO: Use the config settings for region, bucket name, expiration time
     s3 = Aws::S3::Resource.new(region:'us-east-2')
     obj = s3.bucket('pollencam-user-uploads').object(SecureRandom.uuid)
     obj.presigned_url(:put, expires_in: 3.days.to_i)
